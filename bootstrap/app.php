cbb869b93690a68df1c74018c9fea4145da481bb
Murchenko <rabix346@yandex.ru>
2025-05-15 19:35:53 +0300

    chore: add 404 error json api response

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -14,5 +15,8 @@ return Application::configure(basePath: dirname(__DIR__))
         //
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
-    })->create();
+        $exceptions->render(function (NotFoundHttpException $e) {
+            return response()->json(['error' => 'Not found'], 404);
+        });
+    })
+    ->create();
